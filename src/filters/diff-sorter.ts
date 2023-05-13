e3973637113c44f7e4f78bd12e072d6ef7edd083
nyxb <contact@nyxb.xyz>
2023-05-13 21:32:14 +0200

    🔨 refactor(diff-sorter.ts): fix indentation and whitespace 🔨 refactor(dependencies.ts): fix indentation and whitespace The changes in these files are purely cosmetic, fixing indentation and whitespace to improve code readability.

--- src/filters/diff-sorter.ts
+++ src/filters/diff-sorter.ts
@@ -5,5 +5,5 @@ import type { ResolvedDepChange } from '../types'
  * Sort based on the version diff (marjor, minor, etc.)
  */
 export function diffSorter(deps: ResolvedDepChange[]) {
-  return deps.sort((a, b) => DiffMap[a.diff || ''] - DiffMap[b.diff || ''])
+   return deps.sort((a, b) => DiffMap[a.diff || ''] - DiffMap[b.diff || ''])
 }
--- src/io/dependencies.ts
+++ src/io/dependencies.ts
@@ -1,31 +1,31 @@
 import type { DepType, RawDep, ResolvedDepChange } from '../types'
 
 export function parseDependencies(pkg: any, type: DepType, shouldUpdate: (name: string) => boolean): RawDep[] {
-  return Object.entries(pkg[type] || {}).map(([name, version]) => parseDependency(name, version as string, type, shouldUpdate))
+   return Object.entries(pkg[type] || {}).map(([name, version]) => parseDependency(name, version as string, type, shouldUpdate))
 }
 
 export function parseDependency(name: string, version: string, type: DepType, shouldUpdate: (name: string) => boolean): RawDep {
-  return {
-    name,
-    currentVersion: version,
-    source: type,
-    // when `updated` marked to `false`, it will be bypassed on resolving
-    update: shouldUpdate(name),
-  }
+   return {
+      name,
+      currentVersion: version,
+      source: type,
+      // when `updated` marked to `false`, it will be bypassed on resolving
+      update: shouldUpdate(name),
+   }
 }
 
 export function dumpDependencies(deps: ResolvedDepChange[], type: DepType) {
-  const data: Record<string, string> = {}
-  deps
-    .filter(i => i.source === type)
-    .sort((a, b) => a.name.localeCompare(b.name))
-    .forEach((i) => {
-      const version = i.update ? i.targetVersion : i.currentVersion
-      if (i.aliasName === undefined)
-        data[i.name] = version
-      else
-        data[i.aliasName] = `npm:${i.name}${version ? `@${version}` : ''}`
-    })
+   const data: Record<string, string> = {}
+   deps
+      .filter(i => i.source === type)
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .forEach((i) => {
+         const version = i.update ? i.targetVersion : i.currentVersion
+         if (i.aliasName === undefined)
+            data[i.name] = version
+         else
+            data[i.aliasName] = `npm:${i.name}${version ? `@${version}` : ''}`
+      })
 
-  return data
+   return data
 }
